a7ff8508c744da243c218d0ac848ed68fff1e97b
Raghunandan.Seshadri <raghubs81@gmail.com>
2023-09-09 15:24:45 +0530

    Update images

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,13 @@
 # About
 
 DukeGlassNavy is a customisation of the [BottleGlass](https://github.com/etiennegnome/BottleGlass) theme. Inspired by the BottlGlass Dark theme, DukeGlassNavy adds  shades of Navy to the theme.
+
+# ScreenShot
+
+![Cal](/docs/images/cal.jpg)
+
+![Files](/docs/images/files.jpg)
+
+![GEdit](/docs/images/gedit.jpg)
+
+![Picker](/docs/images/picker.jpg)
--- work/build.sh
+++ work/build.sh
@@ -6,7 +6,14 @@ THEMEDIR=$(readlink -f ${BASEDIR}/../)
 echo "Generating DukeGlassNavy.gresource"
 gresource-create gresource DukeGlassNavy
 
+echo ""
 echo "Updating DukeGlassNavy.gresource"
 cp DukeGlassNavy.gresource ${THEMEDIR}/gtk-3.0/gtk.gresource
 ls -l DukeGlassNavy.gresource ${THEMEDIR}/gtk-3.0/gtk.gresource
+
+echo ""
+echo "Build archive"
+cd ${THEMEDIR}
+zip -q DukeGlassNavy.zip -r gnome-shell gtk-3.0 index.theme
+ls -l ${THEMEDIR}/DukeGlassNavy.zip
 
